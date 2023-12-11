e432c4ff6c943e0ab7c32557318eac2d75bf5169
innerstella <suji0509@naver.com>
2023-12-11 22:28:44 +0900

    feat : Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start";
+import MainPage from "./pages/Main";
+import DetailPage from "./pages/Detail";
 import KeywordPage from "./pages/keyword";
 import ResultPage from "./pages/result";
+import CreatePage from "./admin/Create";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/keyword" element={<KeywordPage />} />
-        <Route path="/result" element={<ResultPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+
+        <Route path="/create" element={<CreatePage />} />
+
+        {/* <Route path="/keyword" element={<KeywordPage />} />
+        <Route path="/result" element={<ResultPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
