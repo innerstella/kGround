7af08eb41c1a7765354877d24632d1dbd62a6a75
innerstella <suji0509@naver.com>
2023-12-17 17:39:49 +0900

    style : Adjust padding in Logo component

--- src/pages/Main/Main.style.ts
+++ src/pages/Main/Main.style.ts
@@ -10,6 +10,6 @@ export const MainWrapper = styled.div`
 
 export const Logo = styled.img`
   width: 10rem;
-  padding-top: 1rem;
-  padding-bottom: 2.5rem;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
 `;
