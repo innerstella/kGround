1037974355133e070d27675f3dffd941eef81469
innerstella <suji0509@naver.com>
2023-12-17 23:42:11 +0900

    router : Add KeywordPage to Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import DetailPage from "./pages/Detail";
 import CreatePage from "./admin/Create";
 import RecommendListPage from "./pages/List/RecommendList";
 import SubwayListPage from "./pages/List/SubwayList";
+import KeywordPage from "./pages/Keyword";
 
 const AppRouter = () => {
   return (
@@ -16,6 +17,7 @@ const AppRouter = () => {
         <Route path="/detail" element={<DetailPage />} />
         <Route path="/list/recommendation" element={<RecommendListPage />} />
         <Route path="/list/subway" element={<SubwayListPage />} />
+        <Route path="/keywords" element={<KeywordPage />} />
 
         {/* 관리자 페이지 */}
         <Route path="/create" element={<CreatePage />} />
