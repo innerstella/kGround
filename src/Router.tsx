a6c7d0a063290827955a059f10732f56a4ae7277
innerstella <suji0509@naver.com>
2023-12-17 18:56:12 +0900

    Add new routes for list pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,22 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/Start";
 import MainPage from "./pages/Main";
 import DetailPage from "./pages/Detail";
-import KeywordPage from "./pages/keyword";
-import ResultPage from "./pages/result";
 import CreatePage from "./admin/Create";
+import RecommendListPage from "./pages/List/RecommendList";
+import SubwayListPage from "./pages/List/SubwayList";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 비로그인 접근 가능 */}
         <Route path="/" element={<StartPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/detail" element={<DetailPage />} />
+        <Route path="/list/recommendation" element={<RecommendListPage />} />
+        <Route path="/list/subway" element={<SubwayListPage />} />
 
+        {/* 관리자 페이지 */}
         <Route path="/create" element={<CreatePage />} />
-
-        {/* <Route path="/keyword" element={<KeywordPage />} />
-        <Route path="/result" element={<ResultPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
