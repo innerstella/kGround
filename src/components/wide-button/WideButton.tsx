f9e74daee6d188dc450bdfa2138326067c027620
innerstella <suji0509@naver.com>
2023-12-17 23:43:09 +0900

    feat : Add WideButton component

---
+++ src/components/wide-button/WideButton.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+interface Props {
+  text: string;
+  onClick?: () => void;
+}
+
+const WideButton = ({ text, onClick }: Props) => {
+  return (
+    <MainWrapper onClick={onClick}>
+      <p className="text">{text}</p>
+    </MainWrapper>
+  );
+};
+
+export default WideButton;
+
+const MainWrapper = styled.div`
+  display: inline-flex;
+  padding: 0.875rem 6.3125rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+
+  border-radius: 0.375rem;
+  background: var(--Point-Color, #35b711);
+
+  width: 100%;
+
+  .text {
+    color: var(--white, #fff);
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
