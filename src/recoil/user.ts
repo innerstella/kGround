80fe8243e5edb388c4faa57ee59de9006726688b
innerstella <suji0509@naver.com>
2023-12-18 21:41:04 +0900

    data : userData

---
+++ src/recoil/user.ts
@@ -0,0 +1,39 @@
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+
+export interface UserData {
+  userName: string;
+  userGender: 1 | 2;
+  userBirth: number;
+  userWishList: number[];
+  userReviewList: string[];
+}
+
+export const userState = atom<UserData>({
+  key: "userState",
+  default: {
+    userName: "",
+    userGender: 1,
+    userBirth: 0,
+    userWishList: [],
+    userReviewList: [],
+  },
+
+  effects_UNSTABLE: [persistAtom],
+});
+
+export interface UserLoginData {
+  isLogin: boolean;
+  uid: string;
+}
+
+export const userLoginState = atom<UserLoginData>({
+  key: "userLoginState",
+  default: {
+    isLogin: false,
+    uid: "",
+  },
+  effects_UNSTABLE: [persistAtom],
+});
