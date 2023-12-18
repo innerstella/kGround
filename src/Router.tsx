a1a53069e0d5cf8138c9817140c4d6166fa809f6
innerstella <suji0509@naver.com>
2023-12-18 19:43:17 +0900

    router : MyPage component to Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import CreatePage from "./admin/Create";
 import RecommendListPage from "./pages/List/RecommendList";
 import SubwayListPage from "./pages/List/SubwayList";
 import KeywordPage from "./pages/Keyword";
+import MyPage from "./pages/Mypage";
 
 const AppRouter = () => {
   return (
@@ -18,6 +19,9 @@ const AppRouter = () => {
         <Route path="/list/recommendation" element={<RecommendListPage />} />
         <Route path="/list/subway" element={<SubwayListPage />} />
         <Route path="/keywords" element={<KeywordPage />} />
+
+        {/* 로그인 필요 */}
+        <Route path="/mypage" element={<MyPage />} />
 
         {/* 관리자 페이지 */}
         <Route path="/create" element={<CreatePage />} />
