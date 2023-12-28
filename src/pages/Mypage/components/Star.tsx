f7e14fbb8a8105eabc48f0c0eeb3f2d77ce1ae27
innerstella <suji0509@naver.com>
2023-12-28 11:06:32 +0900

    feat : 별점 UI

--- src/pages/Mypage/components/Star.tsx
+++ src/pages/Mypage/components/Star.tsx
@@ -1,15 +1,93 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Star = () => {
+  const [rateScore, setRateScore] = useState(5);
+  const [star1, setStar1] = useState(false);
+  const [star2, setStar2] = useState(false);
+  const [star3, setStar3] = useState(false);
+  const [star4, setStar4] = useState(false);
+  const [star5, setStar5] = useState(false);
+
+  const icList: { [key: string]: string } = {
+    true: "on",
+    false: "off",
+  };
+
+  const clickStar = (starScore: number) => {
+    setRateScore(starScore);
+    if (starScore === 1) {
+      setRateScore(1);
+      setStar1(true);
+      setStar2(false);
+      setStar3(false);
+      setStar4(false);
+      setStar5(false);
+    } else if (starScore === 2) {
+      setRateScore(2);
+      setStar1(true);
+      setStar2(true);
+      setStar3(false);
+      setStar4(false);
+      setStar5(false);
+    } else if (starScore === 3) {
+      setRateScore(3);
+      setStar1(true);
+      setStar2(true);
+      setStar3(true);
+      setStar4(false);
+      setStar5(false);
+    } else if (starScore === 4) {
+      setRateScore(4);
+      setStar1(true);
+      setStar2(true);
+      setStar3(true);
+      setStar4(true);
+      setStar5(false);
+    } else if (starScore === 5) {
+      setRateScore(5);
+      setStar1(true);
+      setStar2(true);
+      setStar3(true);
+      setStar4(true);
+      setStar5(true);
+    }
+  };
+
   return (
     <MainWrapper>
       <p className="text">여행지에 만족하셨나요?</p>
       <StarWrapper>
-        <img src="/assets/svg/ic-star-on.svg" alt="star" />
-        <img src="/assets/svg/ic-star-on.svg" alt="star" />
-        <img src="/assets/svg/ic-star-on.svg" alt="star" />
-        <img src="/assets/svg/ic-star-on.svg" alt="star" />
-        <img src="/assets/svg/ic-star-off.svg" alt="star" />
+        <img
+          onClick={() => clickStar(1)}
+          className="star"
+          src={`/assets/svg/ic-star-${icList[star1.toString()]}.svg`}
+          alt="star"
+        />
+        <img
+          onClick={() => clickStar(2)}
+          className="star"
+          src={`/assets/svg/ic-star-${icList[star2.toString()]}.svg`}
+          alt="star"
+        />
+        <img
+          onClick={() => clickStar(3)}
+          className="star"
+          src={`/assets/svg/ic-star-${icList[star3.toString()]}.svg`}
+          alt="star"
+        />
+        <img
+          onClick={() => clickStar(4)}
+          className="star"
+          src={`/assets/svg/ic-star-${icList[star4.toString()]}.svg`}
+          alt="star"
+        />
+        <img
+          onClick={() => clickStar(5)}
+          className="star"
+          src={`/assets/svg/ic-star-${icList[star5.toString()]}.svg`}
+          alt="star"
+        />
       </StarWrapper>
     </MainWrapper>
   );
